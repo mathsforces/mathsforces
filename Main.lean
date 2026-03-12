7ea4841a420a9cc584e7ff8ced0e5815451f883e
PCloud <heige.pcloud@outlook.com>
2026-03-12 15:36:38 +0000

    Implement dynamic response handling in MyHandler based on request URI

--- Main.lean
+++ Main.lean
@@ -10,7 +10,14 @@ structure MyHandler
 
 instance : Handler MyHandler where
   onRequest _ req := do
-    Response.ok |>.text "Hello, World!"
+    let uri := req.line.uri
+    let response := match uri with
+    | .originForm path query => s!"originForm {path} {query}"
+    | .absoluteForm uri noFrag => s!"absoluteForm {uri}"
+    | .authorityForm authority => s!"authorityForm {authority}"
+    | .asteriskForm => s!"asteriskFOrm"
+
+    Response.ok |>.text response
 
 public def main : IO Unit := Async.block do
   let addr : Net.SocketAddress := .v4 ⟨.ofParts 127 0 0 1, 8000⟩
